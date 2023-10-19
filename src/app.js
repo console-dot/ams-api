3d9aeb17c1561396e08796162169d15baf3bf540
Abdul Raheem <abdul.raheem.30140@gmail.com>
2023-10-20 01:23:11 +0500

    QR code

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const fileUpload = require('express-fileupload');
 app.use(fileUpload());
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
 
 // API ENDPOINT
 app.use('/api/v1', router);
