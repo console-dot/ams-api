21f5a124e009ea47ad12e2e9af92d0ef3b067c0d
Abdul Raheem <abdul.raheem.30140@gmail.com>
2023-10-26 14:50:43 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 // Static IP Code
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://116.58.56.154');
+  res.header('Access-Control-Allow-Origin', '116.58.56.154');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
