0831d09aebb5ccb19ece84528aa4429018848adf
Shuja Ur Rehman <shuja0094@gmail.com>
2024-07-05 15:28:22 +0500

    Update hrAttendance.js

--- src/routes/hrAttendance.js
+++ src/routes/hrAttendance.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
-const { HrAttendance } = require("../handlers");
+const { HrAttendance, Attendance } = require("../handlers");
 const handler = new HrAttendance();
+const handlerAttendance = new Attendance();
 
 router.get("/all-attendances", handler.getAllAttendance);
 router.get("/search-emp-attendance", handler.searchEmployeeAttendance);
@@ -11,5 +12,6 @@ router.post("/mark-leave", handler.markLeave);
 router.get("/leaves", handler.fetchLeaves);
 router.post("/mark-holiday", handler.markHoliday);
 router.get("/all-leaves", handler.fetchAllLeaves);
+router.put("/update-attendance/:id", handlerAttendance.updateAttendance);
 
 module.exports = router;
